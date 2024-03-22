eedc9167b960008dbfdc512a03cf24a18ac8a4b8
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2024-03-23 02:03:55 +0330

    install treesitter ptyhon

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html", "php" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html", "php", "python" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },

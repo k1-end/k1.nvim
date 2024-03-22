06854957783a8cf913a23da5b2bdba5c08301df0
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2024-03-23 02:12:16 +0330

    make folds open by default

--- lua/k1/settings.lua
+++ lua/k1/settings.lua
@@ -6,14 +6,11 @@ vim.opt.softtabstop = 4 -- how many tabs to insert when pressing tab
 vim.opt.shiftwidth = 4 -- auto-indentation 
 vim.opt.expandtab = true -- replace tabs with spaces
 vim.o.updatetime = 100
-
 vim.opt.smartindent = true
-
-vim.opt.wrap = false
 
 vim.cmd("set foldmethod=indent")
 vim.cmd("colorscheme tokyonight")
-vim.cmd("autocmd BufRead * normal zr")
+vim.cmd("autocmd BufRead * normal zR")
 vim.cmd("autocmd FileType qf nnoremap <buffer> <CR> <CR>:cclose<CR>")
 vim.o.ignorecase = true
 vim.o.wrap = true

9db96655a14d96f0490d5ecae2e4a90b12846b94
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2024-03-23 02:04:16 +0330

    install python language server

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -51,6 +51,6 @@ end
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'intelephense', 'tsserver', 'sqlls', 'lua_ls', 'ltex'},
+  ensure_installed = {'intelephense', 'tsserver', 'sqlls', 'lua_ls', 'ltex', 'pylsp'},
   handlers = {default_setup},
 })

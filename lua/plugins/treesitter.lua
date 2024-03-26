fdfa26f4c33a6233d11126c226eac2be063e3327
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2024-03-26 08:29:36 +0330

    add markdown treesitter grammer

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html", "php", "python" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html", "php", "python", "markdown", "markdown_inline"},
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },

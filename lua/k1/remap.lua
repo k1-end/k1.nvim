e840c2621b9ec4e5611fc5b9fffc7985039b0129
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2024-03-31 11:12:59 +0330

    set keymap for toggling Neotree

--- lua/k1/remap.lua
+++ lua/k1/remap.lua
@@ -4,11 +4,14 @@ vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true 
 
 vim.keymap.set("n", "<leader>nrw", vim.cmd.Ex) -- netrw
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- moving lines
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- moving lines
+
 vim.keymap.set("n", "J", "mzJ`z")            -- joining lines
 vim.keymap.set('n', '<leader>fg', ':G<CR>:Neotree close<CR><C-w><C-o>')
 vim.keymap.set('i', '<C-e>', '<C-o>$')       -- go to the end of the line in insert mode
+vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>")

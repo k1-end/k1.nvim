d60c739fca2118b14183adbbeb909038df162397
Keyvan Poursoltani <keyvan.soltan18@gmail.com>
2024-04-03 23:38:13 +0330

    add telescope search keymaps keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,4 @@ vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch curren
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })

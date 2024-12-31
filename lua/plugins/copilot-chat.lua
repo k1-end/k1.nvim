268c1454d9576d0795c47f50fbaab0a7011bbde0
K1 <keyvan.soltan18@gmail.com>
2024-12-31 11:46:06 +0330

    Add copilot chat

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+            proxy = "http://172.16.51.117:1081"
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}

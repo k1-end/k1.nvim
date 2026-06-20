784013e479fb569354795194d696f6d2882b9542
test <test@test.com>
2026-06-20 22:46:40 +0330

    fix(treesitter): move blade parser config into lazy spec, fix require paths
    - Move blade parser registration from after/plugin/treesitter.lua into
      nvim-treesitter lazy.nvim plugin spec config function
    - Fix 'configs' -> 'config' (actual module name)
    - Remove .get_parser_configs() call (module itself is the config table)
    - Delete after/plugin/treesitter.lua (now handled by lazy spec)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-      local configs = require("nvim-treesitter.configs")
+      local configs = require("nvim-treesitter.config")
 
       configs.setup({
           ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html", "php", "python", "markdown", "markdown_inline"},
@@ -10,5 +10,26 @@ return {
           highlight = { enable = true },
           indent = { enable = true },
         })
+
+      local parser_config = require("nvim-treesitter.parsers")
+      parser_config.blade = {
+          install_info = {
+              url = "https://github.com/EmranMR/tree-sitter-blade",
+              files = { "src/parser.c" },
+              branch = "main",
+          },
+          filetype = "blade",
+      }
+      vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true })
+      vim.api.nvim_create_autocmd("BufNewFile", {
+          group = "BladeFiltypeRelated",
+          pattern = "*.blade.php",
+          callback = function() vim.bo.filetype = "blade" end,
+      })
+      vim.api.nvim_create_autocmd("BufRead", {
+          group = "BladeFiltypeRelated",
+          pattern = "*.blade.php",
+          callback = function() vim.bo.filetype = "blade" end,
+      })
     end
  }

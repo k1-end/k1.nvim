b0b5e6cf5f77ac1778ce2977aa94cb3e4fc278c5
K1 <keyvansoltan49@gmail.com>
2026-06-21 10:13:54 +0330

    fix(treesitter): require nvim-treesitter.configs (plural) for correct module path

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-      local configs = require("nvim-treesitter.config")
+      local configs = require("nvim-treesitter.configs")
 
       configs.setup({
           ensure_installed = { "c", "lua", "vim", "vimdoc","javascript", "html", "php", "python", "markdown", "markdown_inline"},
